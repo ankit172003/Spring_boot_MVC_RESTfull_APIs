82736d1946c505562edfab3b8cf86fb22bb00713
Ankit <akchadha631@gmail.com>
2026-06-18 19:49:41 +0530

    API-Response format standards

--- src/main/java/com/SP/week2/SpringBootWeb/advices/GlobalExceptionHandler.java
+++ src/main/java/com/SP/week2/SpringBootWeb/advices/GlobalExceptionHandler.java
@@ -7,35 +7,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiError> handleResourceNotFound(ResourceNotFoundException exception){
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFound(ResourceNotFoundException exception){
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .message(exception.getMessage())
                 .build();
-        return new ResponseEntity<>(apiError,HttpStatus.NOT_FOUND);
+        return buildResponseEntity(apiError);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiError> handleInternalServerError(Exception exception){
+    public ResponseEntity<ApiResponse<?>> handleInternalServerError(Exception exception){
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .message(exception.getMessage())
                 .build();
-        return new ResponseEntity<>(apiError,HttpStatus.INTERNAL_SERVER_ERROR);
+        return buildResponseEntity(apiError);
 
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiError> handleInputValidationError(MethodArgumentNotValidException exception){
+    public ResponseEntity<ApiResponse<?>>  handleInputValidationError(MethodArgumentNotValidException exception){
         List<String> errors = exception
                 .getBindingResult()
                 .getAllErrors()
@@ -49,6 +47,12 @@ public class GlobalExceptionHandler {
                 .subErrors(errors)
                 .build();
 
-        return new ResponseEntity<>(apiError,HttpStatus.BAD_REQUEST);
+        return buildResponseEntity(apiError);
+    }
+
+
+
+    private ResponseEntity<ApiResponse<?>> buildResponseEntity(ApiError apiError) {
+        return new ResponseEntity<>(new ApiResponse<>(apiError), apiError.getStatus());
     }
 }
