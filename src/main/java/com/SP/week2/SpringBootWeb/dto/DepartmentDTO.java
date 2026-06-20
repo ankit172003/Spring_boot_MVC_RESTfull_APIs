0c649f060aa47c477361788dbee72817b24033bd
Ankit <akchadha631@gmail.com>
2026-06-20 14:58:52 +0530

    added annotation in departmentDTO

--- src/main/java/com/SP/week2/SpringBootWeb/dto/DepartmentDTO.java
+++ src/main/java/com/SP/week2/SpringBootWeb/dto/DepartmentDTO.java
@@ -3,10 +3,14 @@ package com.SP.week2.SpringBootWeb.dto;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
 
 import java.time.LocalDate;
 
@@ -16,13 +20,15 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class DepartmentDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotBlank(message = "")
+    @Range(min = 3,max = 10,message = "The title character should be in range of 3 and 10")
     private String title;
 
+    @AssertTrue(message = "should be true")
     private boolean active;
 
+    @FutureOrPresent(message = "date can be from future or present")
     private LocalDate createdAt;
 }
